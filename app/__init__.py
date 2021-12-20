40fb3784d8b6dfb52cd829eba62a053230da67c1
Congyuan Ji <jcy20@mails.tsinghua.edu.cn>
2021-12-20 16:02:13 +0800

    add CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 # from flask_sqlalchemy import SQLAlchemy
 # from flask_caching import Cache
 
@@ -6,6 +7,7 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object('settings')
+CORS(app, supports_credentials=True)
 
 from app import blueprint_register
 # cache = Cache(app, config={'CACHE_TYPE': 'simple'})
